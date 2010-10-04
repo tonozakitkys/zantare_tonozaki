1858363183b3ed291f27fd2ffe93f99bb2afe945
tonozakitkys@gmail.com <tono@zantare.(none)>
2010-10-04 17:20:03 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZantareTonozaki::Application.config.secret_token = '9c99b3dddbddf107726fa067b700d4d33bcf55680e8b79a9efb60a73b67e25fbaebadf6556ea0d00156d6b87639e328649c95a2214d381dbe95b2ecd382f0dc2'
