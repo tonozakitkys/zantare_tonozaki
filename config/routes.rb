3815127bb081854c76b7132de40ef6334e3484b3
tonozakitkys@gmail.com <tono@zantare.(none)>
2010-10-04 18:02:33 +0900

    add welcome#hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ZantareTonozaki::Application.routes.draw do
+  get "welcome/hallo"
+
+  get "welcome/hello"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -9,6 +13,7 @@ ZantareTonozaki::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+  match 'hello' => 'welcome#hello'
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
